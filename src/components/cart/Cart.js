fe821ae90e3ce596843235f33dc42022e033b938
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-26 23:32:08 +0530

    Initial Components - setup

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Cart.css";
+
+export default function Cart() {
+  return (
+    <main className="section">
+      <p>Main Cart Details Goes Here</p>
+      <p>Total Amount</p>
+      <button>Clear Cart</button>
+    </main>
+  );
+}
