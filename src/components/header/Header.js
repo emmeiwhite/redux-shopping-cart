1d4f6dab88c8d2c3e75cd21956dca2c9e8c7c6f3
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-26 23:52:00 +0530

    Header Mocked

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./Header.css";
 
 export default function Header() {
   return (
     <header>
-      <nav className="section"> Navigation</nav>
+      <nav className="section main-nav">
+        <span className="brand">ReduxCart</span>
+        <div className="cart-items-wrapper">
+          <div className="total-cart-item">4</div>
+        </div>
+      </nav>
     </header>
   );
 }
